8c80b78054da1eab2409fd5d7110f1eea81efdfc
Glagolev Vladimir <37924155+Retrosop@users.noreply.github.com>
2023-12-17 10:01:15 +1000

    Add Object

--- RashetDB/RashetCstFire/RashetCstFire.py
+++ RashetDB/RashetCstFire/RashetCstFire.py
@@ -1,20 +1,47 @@
-﻿import datetime
+﻿from dataclasses import field
+import datetime 
 import pandas as pd
 
-dataFile = pd.read_csv("firemap1.csv")[:100]
-dfFire = dataFile[['Latitude', 'Longitude','Acq_date','Acq_time']]
-dfFire['Acq_daten'] = 20000101
-dfFire['Year'] = 2000
-for index,row in dfFire.iterrows():
-    date_string = str(row['Acq_date'])
-    dfFire[index,'Acq_daten'] = datetime.datetime.strptime (date_string.removesuffix('.0'), '%Y%m%d')
-    dfFire[index, 'Year'] = datetime.datetime.strptime (date_string.removesuffix('.0'), '%Y%m%d').year
-print(dfFire)
+class Fire:
+    longitude = None
+    latitude = None
+    acq_daten = None
+    idote = None
+    rautomap = None
+    rrailmap = None
+    
+    def __init__(self, longitude, latitude, acq_daten,idote,rautomap,rrailmap):
+        self.longitude = longitude
+        self.latitude = latitude
+        self.acq_daten = acq_daten
+        self.idote = idote
+        self.rautomap = rautomap
+        self.rrailmap = rrailmap
+        
+class FireMap:
+        
+    df = None
+    def loadFire(self):
+        dataFile = pd.read_csv("firemapeao.csv")
+        dfFire = dataFile[['Latitude', 'Longitude','Acq_date','Acq_time']]
+        dfFire['Acq_daten'] = pd.to_datetime(dfFire['Acq_time'])
+        dfFire['Year'] = 2000
+        for index,row in dfFire.iterrows():
+              date_string = str(row['Acq_date'])
+              dfFire.at[index,'Acq_daten'] = datetime.datetime.strptime (date_string, '%Y%m%d')
+              dfFire.at[index, 'Year'] = datetime.datetime.strptime (date_string, '%Y%m%d').year
+        print(dfFire)
+        self.df = dfFire
+    def statFire(self):
+        print(self.df.groupby(pd.PeriodIndex(self.df['Acq_daten'], freq="D"))['Year'].count())
+        print(self.df.groupby(pd.PeriodIndex(self.df['Acq_daten'], freq="M"))['Year'].count())
+        print(self.df.groupby(pd.PeriodIndex(self.df['Acq_daten'], freq="Y"))['Year'].count())
 
-# #https://python-lab.ru/metod-pandas-dataframe-count-v-python
-# print(dfFire.count(axis='columns'))
 
-# dfFire['Count_y'] = dfFire.groupby(y)['Date'].transform('size')
-# print(dfFire)
+def main():
+    fireDB = FireMap()
+    fireDB.loadFire()
+    fireDB.statFire()
 
-print(dfFire.groupby(pd.PeriodIndex(dfFire['Acq_daten'], freq="M"))['Year'].count())
+if __name__ == '__main__':
+    main()
