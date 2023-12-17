59376d469e9a765885e1a82d3cf6e4a15055199e
Glagolev Vladimir <37924155+Retrosop@users.noreply.github.com>
2023-12-17 10:25:39 +1000

    Add 1025_171223

--- RashetDB/RashetCstFire/RashetCstFire.py
+++ RashetDB/RashetCstFire/RashetCstFire.py
@@ -21,8 +21,12 @@ class Fire:
 class FireMap:
         
     df = None
-    def loadFire(self):
-        dataFile = pd.read_csv("firemapeao.csv")
+    def loadFire(self, countFire = None):
+        if countFire != None:
+            dataFile = pd.read_csv("firemapeao.csv")[:countFire]
+        else:
+            dataFile = pd.read_csv("firemapeao.csv")
+            
         dfFire = dataFile[['Latitude', 'Longitude','Acq_date','Acq_time']]
         dfFire['Acq_daten'] = pd.to_datetime(dfFire['Acq_time'])
         dfFire['Year'] = 2000
@@ -33,15 +37,24 @@ class FireMap:
         print(dfFire)
         self.df = dfFire
     def statFire(self):
+        print('Статистика за день')
         print(self.df.groupby(pd.PeriodIndex(self.df['Acq_daten'], freq="D"))['Year'].count())
         print(self.df.groupby(pd.PeriodIndex(self.df['Acq_daten'], freq="M"))['Year'].count())
+        print('Статистика за месяц')
         print(self.df.groupby(pd.PeriodIndex(self.df['Acq_daten'], freq="Y"))['Year'].count())
+        print('Статистика за год')
+    def saveStatFire(self):
+        (self.df.groupby(pd.PeriodIndex(self.df['Acq_daten'], freq="D"))['Year'].count()).to_csv(f'fireday.csv', sep=";")
+        (self.df.groupby(pd.PeriodIndex(self.df['Acq_daten'], freq="M"))['Year'].count()).to_csv(f'firemonth.csv', sep=";")
+        (self.df.groupby(pd.PeriodIndex(self.df['Acq_daten'], freq="Y"))['Year'].count()).to_csv(f'fireyear.csv', sep=";")
+        
 
 
 def main():
     fireDB = FireMap()
     fireDB.loadFire()
     fireDB.statFire()
+    fireDB.saveStatFire()
 
 if __name__ == '__main__':
     main()
